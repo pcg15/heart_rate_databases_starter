d504755b689bf117f3091cadf039ff19b1ef4ff4
pcg15 <pcg15@duke.edu>
2018-03-21 14:30:46 -0400

    fixes bugs with app functions

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ def main():
     postInfo()
     getHeartRate()
     getAverage()
-    postIntervalAverage()
+    #postIntervalAverage()
 
 def postInfo():
     """
@@ -15,34 +15,23 @@ def postInfo():
         "user_age": 24, #// in years
         "heart_rate": 78
     }
-    r = requests.post("http://vcm-3569.vm.duke.edu:5000/api/heart_rate", json=data)
-    r_response = r.json()
-    print(r_response.status_code)
+    r = requests.post("http://0.0.0.0:5000/api/heart_rate", json=data)
+    #r_response = r.json()
+    print(r.json())
 
 def getHeartRate():
     """
     Gets heart rate data from server for user
     """
-    r2 = requests.get("http://vcm-3569.vm.duke.edu:5000/api/heart_rate/pcg15@duke.edu")
-    print(r2)
+    r2 = requests.get("http://0.0.0.0:5000/api/heart_rate/pcg15@duke.edu")
+    print(r2.json())
 
 def getAverage():
     """
     Gets average heart rate for user
     """
-    r3 = requests.get("http://vcm-3569.vm.duke.edu:5000/api/heart_rate/average/pcg15@duke.edu")
-    print(r3)
-
-def postIntervalAverage():
-    """
-    Posts average heart rate calculation for user
-    """
-    data = {
-        "user_email": "",
-        "heart_rate_average_since": "2018-03-09 11:00:36.372339" #// date string
-    }
-    r4 = requests.post("http://vcm-3569.vm.duke.edu:5000/api/heart_rate", json=data)
-    print(r4)
+    r3 = requests.get("http://0.0.0.0:5000/api/heart_rate/average/pcg15@duke.edu")
+    print(r3.json())
 
 if __name__ == '__main__':
     main()
