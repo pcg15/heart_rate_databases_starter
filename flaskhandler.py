d047dee087030d3ab0d27814c81e87198a44a13a
pcg15 <pcg15@duke.edu>
2018-03-23 08:23:40 -0400

    add logging to debug

--- flaskhandler.py
+++ flaskhandler.py
@@ -8,6 +8,9 @@ from filtering import time_filtering, rate_filtering
 from tachycardia import tachycardia
 import models
 import numpy as np
+import logging
+logging.basicConfig(filename="flaskhandler.txt", format='%(levelname)s\
+%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
 connect("mongodb://localhost:27017/heart_rate_app")
 app = Flask(__name__)
 """
@@ -41,15 +44,23 @@ def postInfo():
         email = r["user_email"]
         age = r["user_age"]
         heart_rate = r["heart_rate"]
+        logging.info("/heart_rate: data received and extracted from json")
+        logging.debug("/heart_rate: email = " + str(email))
+        logging.debug("/heart_rate: age = " + str(age))
+        logging.debug("/heart_rate: heart rate = " + str(heart_rate))
         time = datetime.datetime.now()
+        logging.debug("/heart_rate: time = " + str(time))
     except:
         raise ValueError("Submit json data for email, age, and heart_rate")
+        logging.warning("/heart_rate: not all data points found or not json")
         return 400
     try:
         user = add_heart_rate(email, heart_rate, time)
+        logging.info("/heart_rate: user found in database")
         print("New heart rate information was added")
     except:
         user = create_user(email, age, heart_rate, time)
+        logging.info("/heart_rate: user was not found and was created")
         print("A new user was created")
     return 200
 
@@ -66,8 +77,11 @@ def getHeartRate(user_email):
     """
     try:
         user = models.User.objects.raw({"_id": user_email}).first()
+        logging.info("/heart_rate/<user_email>: user found")
+        logging.debug("/heart_rate/<user_email>: user = " + str(user.email))
     except:
         raise KeyError("User not in database")
+        logging.warning("/heart_rate/<user_email>: user not found in database")
         return 400
     response = {
         "user_heart_rate": user.heart_rate
@@ -87,9 +101,15 @@ def getAverage(user_email):
     """
     try:
         user = models.User.objects.raw({"_id": user_email}).first()
+        logging.info("/heart_rate/average/<user_email>: user found")
+        logging.debug("/heart_rate/average/<user_email>: user = "
+                      + str(user.email))
         average_heart_rate = average_hr(user.heart_rate)
+        logging.debug("/heart_rate/avaerae/<user_email>: average = "
+                      + str(average_heart_rate))
     except:
         raise KeyError("User not in database")
+        logging.warning("/heart_rate/<user_email>: user not found in database")
         return 400
     response = {
         "average_heart_rate": average_heart_rate
@@ -114,32 +134,60 @@ def postIntervalAverage():
         email = r["user_email"]
         date = datetime.strptime(r["heart_rate_average_since"],
                                  '%Y-%m-%d %H:%M:%S.%f')
+        logging.info("/heart_rate/interval_average: data received and\
+                     extracted from json")
+        logging.debug("heart_rate/interval_average: email = " + str(email))
+        logging.debug("heart_rate/interval_average: date = " + str(date))
     except:
         raise ValueError("Submit json data for email, age, and heart_rate")
+        logging.warning("heart_rate/interval_average: not all data points\
+                        found or not json")
         return 400
     try:
         user = models.User.objects.raw({"_id": email}).first()
+        logging.info("/heart_rate/interval_average: user found")
     except:
         raise KeyError("User not in database")
+        logging.warning("heart_rate/interval_average: user not found")
         return 400
     try:
         times = time_conversion(user.heart_rate_times)
         filtered_times = time_filtering(times, date)
         rates_index = np.where(times == filtered_times)[0]
+        logging.info("/heart_rate/interval_average: times found and filtered")
+        logging.debug("heart_rate/interval_average: times = " + str(times))
+        logging.debug("heart_rate/interval_average: filtered_times = " \
+                      + str(filtered_times))
+        logging.debug("heart_rate/interval_average: rates_index = " \
+                      + str(rates_index))
     except:
         raise KeyError("No time data found for user")
+        logging.warning("heart_rate/interval_average: time data not found")
         return 400
     try:
         rates = user.heart_rate
         filtered_rates = rate_filtering(rates, rates_index)
         average_heart_rate = average_hr(filtered_rates)
+        logging.info("/heart_rate/interval_average: average heart rate\
+                     calculated")
+        logging.debug("heart_rate/interval_average: rates = " + str(rates))
+        logging.debug("heart_rate/interval_average: filtered_rates = " \
+                      + str(filtered_rates))
+        logging.debug("heart_rate/interval_average: average_heart_rate = " \
+                      + str(average_heart_rate))
     except:
         raise KeyError("No heart rate data found for user")
+        logging.warning("heart_rate/interval_average: hr data not found")
         return 400
     try:
         is_tachycardic = tachycardia(user.age, average_heart_rate)
+        logging.info("/heart_rate/interval_average: tachycardia risk\
+                     calculated")
+        logging.debug("heart_rate/interval_average: is_tachycardic = "\
+                      + str(is_tachycardic))
     except:
         raise KeyError("User age not found in database")
+        logging.warning("heart_rate/interval_average: age not in database")
         return 400
     response = {
         "average_heart_rate": average_heart_rate,
